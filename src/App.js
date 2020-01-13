0753b3acd0547de4bd403f63e2c1de0faef451ae
Wilson <wjunior_msn@hotmail.com>
2020-01-13 09:27:44 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h5>App js</h5>
+    </div>
+  );
+}
+
+export default App;
